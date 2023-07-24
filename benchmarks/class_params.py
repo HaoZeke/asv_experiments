1df18b570ba373a7239b7b85678971c1ae5424a2
Rohit Goswami <rog32@hi.is>
2023-07-24 01:20:07 +0000

    ENH: [example] parameterize_class_with

--- benchmarks/class_params.py
+++ benchmarks/class_params.py
@@ -1,6 +1,23 @@
 import numpy as np
-from asv_runner.benchmarks.mark import *
+from asv_runner.benchmarks.mark import parameterize_class_with
 
+# class TimeSuite:
+#     params = [10, 100, 1000]
+#     param_names = ["size"]
+#     def setup(self, size):
+#         self.d = {}
+#         for x in range(size):
+#             self.d[x] = None
+
+#     def time_keys(self, size):
+#         for key in self.d.keys():
+#             pass
+
+#     def time_values(self, size):
+#         for value in self.d.values():
+#             pass
+
+@parameterize_class_with({"size": [10, 100, 1000]})
 class TimeSuite:
     params = [10, 100, 1000]
     param_names = ["size"]
