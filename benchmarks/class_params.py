08a56cadfcdfe5ab08c42ca217db04c39cb84ea2
Rohit Goswami <rog32@hi.is>
2023-07-24 01:21:52 +0000

    MAINT: Cleanup example

--- benchmarks/class_params.py
+++ benchmarks/class_params.py
@@ -1,26 +1,24 @@
 import numpy as np
 from asv_runner.benchmarks.mark import parameterize_class_with
 
-# class TimeSuite:
-#     params = [10, 100, 1000]
-#     param_names = ["size"]
-#     def setup(self, size):
-#         self.d = {}
-#         for x in range(size):
-#             self.d[x] = None
-
-#     def time_keys(self, size):
-#         for key in self.d.keys():
-#             pass
-
-#     def time_values(self, size):
-#         for value in self.d.values():
-#             pass
-
-@parameterize_class_with({"size": [10, 100, 1000]})
-class TimeSuite:
-    params = [10, 100, 1000]
+class TimeSuiteStandardSingle:
+    params = [10, 100, 200]
     param_names = ["size"]
+    def setup(self, size):
+        self.d = {}
+        for x in range(size):
+            self.d[x] = None
+
+    def time_keys(self, size):
+        for key in self.d.keys():
+            pass
+
+    def time_values(self, size):
+        for value in self.d.values():
+            pass
+
+@parameterize_class_with({"size": [10, 100, 200]})
+class TimeSuiteDecoratorSingle:
     def setup(self, size):
         self.d = {}
         for x in range(size):
